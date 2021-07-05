c326f1afc40605ea494088f733f31e3e73ab2ec4
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2021-07-06 00:33:08 +0900

    add 1806

--- Boj/1806_부분합.py
+++ Boj/1806_부분합.py
@@ -7,7 +7,7 @@ N, M = map(int, input().split())
 numbers = list(map(int, input().split()))
 start = end = 0
 sums = numbers[end]
-min_length = 100000 # 100000000 으로 설정한 경우 답은 맞겠지만 의미가 하나도 없는 변수값 설정
+min_length = 100000
 while (start <= end):
     if (sums >= M):
         min_length = min(min_length, end - start + 1)
