4ba2d4846356f57ac53201e455986235f6a43295
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2021-10-20 16:03:11 +0900

    add 15664

---
+++ Boj/N과M/10.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<string>
+using namespace std;
+vector<int> v, v2;
+map<string, int> check;
+int n, m, num;
+void brute(int idx, int cnt, string s) {
+	if (cnt == m) {
+		if (!check[s]) {
+			check[s] = 1;
+			for (int i = 0; i < m; ++i)
+				cout << v2[i] << " ";
+			cout << "\n";
+		}
+		return;
+	}
+	for (int i = idx; i < n; ++i) {
+			v2.push_back(v[i]);
+			brute(i + 1, cnt + 1,s + to_string(v[i]));
+			v2.pop_back();
+	}
+}
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> n >> m;
+	for (int i = 1; i <= n; ++i) {
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	brute(0, 0, "");
+}
