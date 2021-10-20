0a0c4e83abca6f0346ccd745af1e5276d4cd724a
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2021-10-20 16:01:08 +0900

    add 15652

---
+++ Boj/N과M/4.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<int> v, v2;
+int n, m;
+void brute(int idx, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; ++i)
+			cout << v2[i] << " ";
+		cout << "\n";
+		return;
+	}
+	for (int i = idx; i < n; ++i) {
+		v2.push_back(v[i]);
+		brute(i, cnt + 1);
+		v2.pop_back();
+	}
+}
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> n >> m;
+	for (int i = 1; i <= n; ++i)
+		v.push_back(i);
+	sort(v.begin(), v.end());
+	brute(0, 0);
+}
