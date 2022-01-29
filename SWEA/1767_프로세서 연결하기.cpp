a7ee8c32c6d1f52f0f47394ae58d9461a8431ab6
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2022-01-29 16:17:01 +0900

    add 1767

--- SWEA/1767_프로세서 연결하기.cpp
+++ SWEA/1767_프로세서 연결하기.cpp
@@ -1,70 +1,71 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
+#include<vector>
+#include<climits>
 #define mp make_pair
 using namespace std;
-vector<pair<int, int> > v;
+vector<pair<int, int> >v;
 int map[13][13];
 int dx[4] = { 0,0,-1,1 };
 int dy[4] = { -1,1,0,0 };
-int t, n, result_core, result_len;
-int check(int x, int y, int dir) {
-	for (int i = 1;; ++i) {
-		int nx = x + dx[dir] * i;
-		int ny = y + dy[dir] * i;
-		if (1 > nx || nx > n || 1 > ny || ny > n) return i - 1;
-		if (map[nx][ny]) break;
-	}
-	return 0;
+int result_len, result_core, t, n;
+int make_len(int now_x, int now_y, int dir) {
+    int nx, ny;
+    int cnt_len = 0;
+    for (int i = 1;; ++i) {
+        nx = now_x + dx[dir] * i;
+        ny = now_y + dy[dir] * i;
+        if (0 < nx && nx <= n && 0 < ny && ny <= n) {
+            if (!map[nx][ny]) ++cnt_len;
+            else return -1;
+        }
+        else return cnt_len;
+    }
 }
-void make_len(int x, int y, int dir, int num,int len) {
-	for (int i = 1;i<=len; ++i) {
-		int nx = x + dx[dir] * i;
-		int ny = y + dy[dir] * i;
-		map[nx][ny] = num;
-	}
+void brute(int cnt_core, int cnt_len, int index) {
+    if (index == v.size()) {
+        if (result_core <= cnt_core) {
+            if (result_core != cnt_core) result_len = cnt_len;
+            else result_len = min(result_len, cnt_len);
+            result_core = cnt_core;
+        }
+        return;
+    }
+    int x, y, temp;
+    for (int i = index; i < v.size(); ++i) {
+        x = v[i].first;
+        y = v[i].second;
+        for (int j = 0; j < 4; ++j) {
+            temp = make_len(x, y, j);
+            if (temp != -1) {
+                for (int k = 1; k <= temp; ++k) map[x + dx[j] * k][y + dy[j] * k] = 2;
+                brute(cnt_core + 1, cnt_len + temp, i + 1);
+                for (int k = 1; k <= temp; ++k) map[x + dx[j] * k][y + dy[j] * k] = 0;
+            }
+        }
+    }
 }
-void dfs(int core_cnt, int len_cnt, int index) {
-	if (index == v.size()) {
-		if (result_core < core_cnt) {
-			result_core = core_cnt;
-			result_len = len_cnt;
-		}
-		else if (result_core == core_cnt)
-			result_len = min(result_len, len_cnt);
-		return;
-	}
-	int x = v[index].first;
-	int y = v[index].second;
-	int len;
-	for (int i = 0; i < 4; ++i) {
-		len = check(x, y, i);
-		if (len) {
-			make_len(x, y, i, 2,len);
-			dfs(core_cnt + 1, len_cnt + len, index + 1);
-			make_len(x, y, i, 0,len);
-		}
-	}
-	dfs(core_cnt, len_cnt, index + 1);
-	return;
+void init() {
+    result_len = INT_MAX;
+    result_core = -INT_MAX;
+    v.clear();
 }
 int main() {
-	ios::sync_with_stdio(0); cin.tie(0);
-	cin >> t;
-	for (int tc = 1; tc <= t; ++tc) {
-		cin >> n;
-		result_core = -987654321, result_len = 987654321;
-		v.clear();
-		for (int i = 1; i <= n; ++i) {
-			for (int j = 1; j <= n; ++j) {
-				cin >> map[i][j];
-				if (map[i][j]) {
-					if (i == 1 || j == 1 || i == n || j == n) continue;
-					v.push_back(mp(i, j));
-				}
-			}
-		}
-		dfs(0, 0, 0);
-		cout << "#" << tc << " " << result_len << "\n";
-	}
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> t;
+    for (int l = 1; l <= t; ++l) {
+        cin >> n;
+        init();
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                cin >> map[i][j];
+                if (map[i][j]) {
+                    if (i == 1 || i == n || j == 1 || j == n) continue;
+                    v.push_back(mp(i, j));
+                }
+            }
+        }
+        brute(0, 0, 0);
+        cout << "#" << l << " " << result_len << "\n";
+    }
 }
