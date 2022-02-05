c32f3fce66094ad797374a44e9fdb1e8f249a9b2
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2022-02-05 14:52:59 +0900

    modify 17070

--- Boj/17070_파이프 옮기기1.cpp
+++ Boj/17070_파이프 옮기기1.cpp
@@ -32,3 +32,44 @@ int main() {
 	dfs(0, 1, 2);
 	cout << result;
 }
+
+/*
+#include<iostream>
+using namespace std;
+int map[17][17];
+int dx[3] = { 0,1,1 };
+int dy[3] = { 1,0,1 };
+int n;
+int result = 0;
+bool check(int x, int y) {
+	for (int i = 0; i < 2; ++i) {
+		int nx = x - dx[i];
+		int ny = y - dy[i];
+		if (1 > nx || nx > n || 1 > ny || ny > n || map[nx][ny]) return false;
+	}
+	return true;
+}
+void dfs(int x, int y, int dir) {
+	if (x == n && y == n) {
+		++result;
+		return;
+	}
+	for (int i = 0; i < 3; ++i) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if ((dir == 0 && i == 1) || (dir == 1 && i == 0)) continue;
+		if (i == 2 && !check(nx, ny)) continue;
+		if (0 < nx && nx <= n && 0 < ny && ny <= n && !map[nx][ny]) dfs(nx, ny, i);
+	}
+}
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> n;
+	for (int i = 1; i <= n; ++i)
+		for (int j = 1; j <= n; ++j)
+			cin >> map[i][j];
+	dfs(1, 2, 0);
+	cout << result;
+}
+*/
+
