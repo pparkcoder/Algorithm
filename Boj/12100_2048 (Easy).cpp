102232c5679c8ef0583f0a649fc535d5a64096b6
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2022-02-13 18:46:17 +0900

    add 12100

--- Boj/12100_2048 (Easy).cpp
+++ Boj/12100_2048 (Easy).cpp
@@ -6,67 +6,58 @@
 #include<algorithm>
 using namespace std;
 vector<int> v;
-int map[21][21];
-int cmap[21][21];
-int visit[21][21];
+int cmap[21][21], map[21][21];
+bool visit[21][21];
 int dx[4] = { 0,0,-1,1 };
 int dy[4] = { -1,1,0,0 };
-int result = -987654321;
-int n, dir;
-void move(int now_x, int now_y) {
-	int x = now_x;
-	int y = now_y;
-	while(1){
-		int nx = x + dx[dir];
-		int ny = y + dy[dir];
+int n;
+int result = 0;
+void move(int x, int y, int dir) {
+	int now_x = x, now_y = y;
+	while (1) {
+		int nx = now_x + dx[dir];
+		int ny = now_y + dy[dir];
 		if (1 > nx || nx > n || 1 > ny || ny > n) return;
-		if (cmap[nx][ny] != cmap[x][y]) {
-			if (cmap[nx][ny]) return;
-			else {
-				cmap[nx][ny] = cmap[x][y];
-				cmap[x][y] = 0;
-			}
-		}
-		else {
-			if (!visit[nx][ny]) {
-				cmap[nx][ny] *= 2;
-				cmap[x][y] = 0;
+		if (map[nx][ny]) {
+			if (map[nx][ny] == map[now_x][now_y] && !visit[nx][ny]) {
+				map[nx][ny] *= 2;
+				map[now_x][now_y] = 0;
 				visit[nx][ny] = 1;
-			}
-			else {
-				cmap[nx - dx[dir]][ny - dy[dir]] = cmap[x][y];
 			}
 			return;
 		}
-		x = nx;
-		y = ny;
+		else {
+			map[nx][ny] = map[now_x][now_y];
+			map[now_x][now_y] = 0;
+		}
+		now_x = nx, now_y = ny;
 	}
 }
 void brute(int cnt) {
 	if (cnt == 5) {
-		for (int i = 1; i <= n; i++)
-			for (int j = 1; j <= n; j++)
-				cmap[i][j] = map[i][j];
-		for (int i = 0; i < v.size(); i++) {
+		for (int i = 1; i <= n; ++i)
+			for (int j = 1; j <= n; ++j)
+				map[i][j] = cmap[i][j];
+		for (int k = 0; k < 5; ++k) {
 			memset(visit, 0, sizeof(visit));
-			dir = v[i];
+			int dir = v[k];
 			if (dir == 0 || dir == 2) {
-				for (int k = 1; k <= n; k++)
-					for (int j = 1; j <= n; j++)
-						if(cmap[k][j]) move(k, j);
+				for (int i = 1; i <= n; ++i)
+					for (int j = 1; j <= n; ++j)
+						if(map[i][j]) move(i, j, dir);
 			}
 			else {
-				for (int k = n; k > 0; k--)
-					for (int j = n; j > 0; j--)
-						if (cmap[k][j]) move(k, j);
+				for (int i = n; i > 0; --i)
+					for (int j = n; j > 0; --j)
+						if (map[i][j]) move(i, j, dir);
 			}
+			for (int i = 1; i <= n; ++i)
+				for (int j = 1; j <= n; ++j)
+					result = max(result, map[i][j]);
 		}
-		for (int k = 1; k <= n; k++)
-			for (int j = 1; j <= n; j++)
-				result = max(result, cmap[k][j]);
 		return;
 	}
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < 4; ++i) {
 		v.push_back(i);
 		brute(cnt + 1);
 		v.pop_back();
@@ -75,9 +66,9 @@ void brute(int cnt) {
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	cin >> n;
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= n; j++)
-			cin >> map[i][j];
+	for (int i = 1; i <= n; ++i)
+		for (int j = 1; j <= n; ++j)
+			cin >> cmap[i][j];
 	brute(0);
 	cout << result;
 }
