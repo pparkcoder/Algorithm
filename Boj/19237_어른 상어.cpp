c905d161a32af6a377b0cbcf2a1e8daf7e9392aa
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2022-03-05 15:48:19 +0900

    19237

--- Boj/19237_어른 상어.cpp
+++ Boj/19237_어른 상어.cpp
@@ -6,105 +6,111 @@
 #define mp make_pair
 using namespace std;
 typedef struct {
-	int x, y, dir;
+	int num, x, y, dir, life;
 }info;
 info I;
-vector<pair<int, int> > map[21][21];
 vector<info> v;
-vector<int> priority[400][5];
-int dx[5] = { 0,-1,1,0,0 };
-int dy[5] = { 0,0,0,-1,1 };
-int n, m, k, num;
+vector<pair<int, int> > map[21][21]; // 번호,시간
+vector<int> priority[400][4];
+int dx[4] = { -1,1,0,0 };
+int dy[4] = { 0,0,-1,1 };
+int n, m, k;
+bool cmp(info a, info b) {
+	if (a.num < b.num) return true;
+	else return false;
+}
 bool check() {
-	for (int i = 1; i < v.size(); ++i)
-		if (v[i].x != -1) return false;
+	int temp;
+	for (int i = 0; i < m; ++i) {
+		if (!v[i].life) continue;
+		int x = v[i].x;
+		int y = v[i].y;
+		int num = v[i].num;
+		map[x][y].push_back(mp(num, k + 1));
+	}
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			temp = map[i][j].size();
+			if (temp) {
+				if (temp > 1) {
+					sort(map[i][j].begin(), map[i][j].end());
+					if (map[i][j][0].first == map[i][j][1].first) map[i][j][0].second = k + 1;
+					else {
+						for (int l = 1; l < temp; ++l)
+							v[map[i][j][l].first - 1].life = 0;
+					}
+					map[i][j].erase(map[i][j].begin() + 1, map[i][j].end());
+				}
+				if (--map[i][j][0].second <= 0) map[i][j].clear();
+			}
+		}
+	}
+	for (int i = 1; i < m; ++i)
+		if (v[i].life) return false;
 	return true;
 }
 int start() {
-	int x, y, dir, nx, ny, ndir, flag;
-	for (int t = 0; t <= 1000; ++t) {
-		for (int i = 0; i < v.size(); ++i) {
-			if (check()) return t;
+	int num, x, y, dir, nx, ny, ndir, flag;
+	for (int l = 1; l < 1001; ++l) {
+		for (int i = 0; i < m; ++i) {
+			if (!v[i].life) continue;
+			num = v[i].num;
 			x = v[i].x;
 			y = v[i].y;
 			dir = v[i].dir;
 			flag = 0;
-			if (x == -1) continue;
-			for (int j = 0; j < 4; ++j) {
-				ndir = priority[i][dir - 1][j];
-				nx = x + dx[ndir];
-				ny = y + dy[ndir];
-				if (0 < nx && nx <= n && 0 < ny && ny <= n && !map[nx][ny].size()) {
-					flag = 1;
+			for (int temp = 0; temp < 2; ++temp) {
+				for (int j = 0; j < 4; ++j) {
+					ndir = priority[i][dir][j];
+					nx = x + dx[ndir];
+					ny = y + dy[ndir];
+					if (0 < nx && nx <= n && 0 < ny && ny <= n) {
+						if (!temp && !map[nx][ny].size()) {
+							flag = 1;
+							break;
+						}
+						else if (temp && map[nx][ny][0].first == num) {
+							flag = 1;
+							break;
+						}
+					}
+				}
+				if (flag) {
+					v[i].x = nx;
+					v[i].y = ny;
+					v[i].dir = ndir;
 					break;
 				}
 			}
-			if (!flag) {
-				for (int j = 0; j < 4; ++j) {
-					ndir = priority[i][dir - 1][j];
-					nx = x + dx[ndir];
-					ny = y + dy[ndir];
-					if (0 < nx && nx <= n && 0 < ny && ny <= n && map[nx][ny].front().first == i + 1) break;
-				}
-			}
-			v[i].x = nx;
-			v[i].y = ny;
-			v[i].dir = ndir;
 		}
-		for (int i = 1; i <= n; ++i) {
-			for (int j = 1; j <= n; ++j) {
-				if (map[i][j].size() && --map[i][j].front().second <= 0)
-					map[i][j].clear();
-			}
-		}
-		for (int i = 0; i < v.size(); ++i) {
-			x = v[i].x;
-			y = v[i].y;
-			if (x == -1) continue;
-			map[x][y].push_back(mp(i + 1, k));
-		}
-		vector<int> temp;
-		for (int i = 1; i <= n; ++i) {
-			for (int j = 1; j <= n; ++j) {
-				if (map[i][j].size() > 1) {
-					for (int l = 0; l < map[i][j].size(); ++l)
-						temp.push_back(map[i][j][l].first);
-					sort(temp.begin(), temp.end());
-					for (int l = 1; l < map[i][j].size(); ++l)
-						if (temp[l] != temp.front())	v[temp[l] - 1].x = -1;
-					map[i][j].clear();
-					map[i][j].push_back(mp(temp.front(), k));
-					temp.clear();
-				}
-			}
-		}
+		if (check()) return l;
 	}
 	return -1;
 }
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
-	vector<pair<int, pair<int, int> > > temp;
+	int num, dir;
 	cin >> n >> m >> k;
 	for (int i = 1; i <= n; ++i) {
 		for (int j = 1; j <= n; ++j) {
 			cin >> num;
-			if (1 <= num && num <= m) {
+			if (num) {
 				map[i][j].push_back(mp(num, k));
-				temp.push_back(mp(num, mp(i, j)));
+				I = { num,i,j,0,1 };
+				v.push_back(I);
 			}
 		}
 	}
-	sort(temp.begin(), temp.end());
+	sort(v.begin(), v.end(), cmp);
 	for (int i = 0; i < m; ++i) {
-		cin >> num;
-		I = { temp[i].second.first,temp[i].second.second,num };
-		v.push_back(I);
+		cin >> dir;
+		v[i].dir = dir - 1;
 	}
 	for (int i = 0; i < m; ++i) {
 		for (int j = 0; j < 4; ++j) {
 			for (int l = 0; l < 4; ++l) {
-				cin >> num;
-				priority[i][j].push_back(num);
+				cin >> dir;
+				priority[i][j].push_back(dir - 1);
 			}
 		}
 	}
