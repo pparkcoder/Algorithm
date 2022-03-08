c348cc50142ca5110c7c3167e29e2123de1e4ff0
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2022-03-08 19:35:44 +0900

    modify 20056

--- Boj/20056_마법사 상어와 파이어볼.cpp
+++ Boj/20056_마법사 상어와 파이어볼.cpp
@@ -11,82 +11,77 @@ typedef struct {
 typedef struct {
 	int m, s, d;
 }info2;
-info I;
-info2 I2;
-vector<info2> map[51][51];
+info INFO;
+info2 INFO2;
 vector<info> v;
-vector<int> temp;
+vector<info2> map[51][51];
 int dx[8] = { -1,-1,0,1,1,1,0,-1 };
 int dy[8] = { 0,1,1,1,0,-1,-1,-1 };
-int N, M, t, r, c, m, s, d;
+int N, M, k, r, c, m, s, d;
+int result = 0;
+void check() {
+	v.clear();
+	int temp_size, temp_d, sum_m, sum_s, flag;
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= N; ++j) {
+			temp_size = map[i][j].size();
+			if (temp_size) {
+				if (temp_size == 1) {
+					INFO = { i,j,map[i][j][0].m,map[i][j][0].s ,map[i][j][0].d };
+					v.push_back(INFO);
+				}
+				else {
+					flag = sum_m = sum_s = 0;
+					temp_d = map[i][j][0].d % 2;
+					for (int l = 0; l < temp_size; ++l) {
+						sum_m += map[i][j][l].m;
+						sum_s += map[i][j][l].s;
+						if (l && !flag && map[i][j][l].d % 2 != temp_d) flag = 1;
+					}
+					if (sum_m / 5) {
+						for (int l = flag; l < 8; l += 2) {
+							INFO = { i,j,sum_m / 5,sum_s / temp_size,l };
+							v.push_back(INFO);
+						}
+					}
+				}
+				map[i][j].clear();
+			}
+		}
+	}
+}
 void start() {
-	int result = 0;
-	int len, ns, nr, nc, temp_m, temp_s, dir;
-	while (t--) {
+	int ns, nr, nc;
+	for (int l = 1; l <= k; ++l) {
 		for (int i = 0; i < v.size(); ++i) {
 			r = v[i].r;
 			c = v[i].c;
 			m = v[i].m;
 			s = v[i].s;
 			d = v[i].d;
-			ns = s % N; // 이동 후 최종 위치를 결정
+			ns = s % N;
 			nr = r + dx[d] * ns;
 			nc = c + dy[d] * ns;
-			if (nr < 1) nr += N;
-			else if (nr > N) nr -= N;
-			if (nc < 1) nc += N;
-			else if (nc > N) nc -= N;
-			I2 = { m,s,d };
-			map[nr][nc].push_back(I2);
+			if (nr > N) nr -= N;
+			else if (nr < 1) nr += N;
+			if (nc > N) nc -= N;
+			else if (nc < 1) nc += N;
+			INFO2 = { m,s,d };
+			map[nr][nc].push_back(INFO2);
 		}
-		v.clear();
-		for (int i = 1; i <= N; ++i) {
-			for (int j = 1; j <= N; ++j) {
-				len = map[i][j].size();
-				if (len >= 1) {
-					if (len > 1) {
-						temp_m = temp_s = dir = 0;
-						temp.clear();
-						for (int k = 0; k < len; ++k) {
-							temp_m += map[i][j][k].m;
-							temp_s += map[i][j][k].s;
-							temp.push_back(map[i][j][k].d);
-						}
-						for (int k = 1; k < temp.size(); ++k) {
-							if (temp[k - 1] % 2 != temp[k] % 2) {
-								dir = 1;
-								break;
-							}
-						}
-						temp_m /= 5;
-						temp_s /= len;
-						if (temp_m) {
-							for (int k = 0; k < 4; ++k, dir += 2) {
-								I = { i,j,temp_m,temp_s,dir };
-								v.push_back(I);
-							}
-						}
-					}
-					else {
-						I = { i,j,map[i][j][0].m,map[i][j][0].s,map[i][j][0].d };
-						v.push_back(I);
-					}
-					map[i][j].clear();
-				}
-			}
-		}
+		check();
 	}
-	for (int i = 0; i < v.size(); ++i) 
+	for (int i = 0; i < v.size(); ++i)
 		result += v[i].m;
 	cout << result;
 }
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
-	cin >> N >> M >> t;
-	while (M--) {
+	cin >> N >> M >> k;
+	for (int i = 0; i < M; ++i) {
 		cin >> r >> c >> m >> s >> d;
-		I = { r,c,m,s,d };
-		v.push_back(I);
+		INFO = { r,c,m,s,d };
+		v.push_back(INFO);
 	}
 	start();
 }
