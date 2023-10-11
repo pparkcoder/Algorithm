0ee2ef5398f8567eda6065f580191b6f850e8f82
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2023-10-11 21:36:33 +0900

    add code file

--- 코드트리/싸움땅.cpp
+++ 코드트리/싸움땅.cpp
@@ -111,3 +111,119 @@ int main() {
 	}
 	start();
 }
+
+// 2번째 풀이
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#define mp make_pair
+using namespace std;
+typedef struct {
+	int x, y, d, s, g;
+}info;
+info I;
+vector<info> v;
+vector<int> gun[21][21];
+int people[21][21], score[31];
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,1,0,-1 };
+int n, m, k;
+int turn(int dir) {
+	if (dir < 2) return dir + 2;
+	else return dir - 2;
+}
+void getGun(int x, int y, int num) {
+	vector<pair<int, int> >temp;
+	int len = gun[x][y].size();
+	if (len) {
+		sort(gun[x][y].begin(), gun[x][y].end());
+		if (gun[x][y][len - 1] > v[num].g) {
+			int temp = gun[x][y][len - 1];
+			gun[x][y][len - 1] = v[num].g;
+			v[num].g = temp;
+		}
+	}
+	v[num].x = x;
+	v[num].y = y;
+	people[x][y] = num;
+}
+void fight(int x, int y, int p1, int p2) { // p1 : 새로온 놈, p2 : 원래 있던 놈
+	int win, lose, nx, ny, nd;
+	int sum1 = v[p1].s + v[p1].g;
+	int sum2 = v[p2].s + v[p2].g;
+	if (sum1 > sum2) win = p1, lose = p2;
+	else if (sum1 < sum2) win = p2, lose = p1;
+	else {
+		if (v[p1].s > v[p2].s) win = p1, lose = p2;
+		else win = p2, lose = p1;
+	}
+	score[win] += abs(sum1 - sum2);
+	// 진놈
+	gun[x][y].push_back(v[lose].g);
+	v[lose].g = 0;
+	nd = v[lose].d;
+	for (int i = 0; i < 4; ++i) {
+		nx = x + dx[nd];
+		ny = y + dy[nd];
+		if (1 > nx || nx > n || 1 > ny || ny > n || people[nx][ny]) {
+			if (++nd > 3) nd = 0;
+		}
+		else {
+			v[lose].d = nd;
+			getGun(nx, ny, lose);
+			break;
+		}
+	}
+	// 이긴놈
+	getGun(x, y, win);
+}
+void start() {
+	while (k--) {
+		for (int i = 1; i < v.size(); ++i) {
+			int x = v[i].x;
+			int y = v[i].y;
+			int d = v[i].d;
+			int s = v[i].s;
+			int g = v[i].g;
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			int nd = d;
+			people[x][y] = 0;
+			if (1 > nx || nx > n || 1 > ny || ny > n) {
+				nd = turn(nd);
+				nx = x + dx[nd];
+				ny = y + dy[nd];
+				v[i].d = nd;
+			}
+			if (people[nx][ny]) {
+				fight(nx, ny, i, people[nx][ny]);
+			}
+			else {
+				getGun(nx, ny, i);
+			}
+		}
+	}
+	for (int i = 1; i <= m; ++i)
+		cout << score[i] << " ";
+}
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int x, y, d, s, num;
+	cin >> n >> m >> k;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			cin >> num;
+			if (num) gun[i][j].push_back(num);
+		}
+	}
+	I = { -1,-1,-1,-1,-1 };
+	v.push_back(I);
+	for (int i = 1; i <= m; ++i) {
+		cin >> x >> y >> d >> s;
+		I = { x,y,d,s,0 };
+		v.push_back(I);
+		people[x][y] = i;
+	}
+	start();
+}
