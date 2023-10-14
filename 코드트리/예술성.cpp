0d12f084abfc175362edad4261929bd990820e87
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2023-10-14 17:41:38 +0900

    Update code file

--- 코드트리/예술성.cpp
+++ 코드트리/예술성.cpp
@@ -142,3 +142,153 @@ int main() {
 	}
 		cout << result;
 }
+
+// second
+// map을 이용하여 맞닿은 변의 개수 계산
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+#include<cstring>
+#define mp make_pair
+using namespace std;
+vector<pair<int, int> > group; // 숫자, 칸의 개수
+int board[30][30], visit[30][30];
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { -1,1,0,0 };
+int n, group_num;
+int result = 0;
+void turn() {
+	vector<int> v;
+	int limit = (n / 2) + 1, index = 0;
+	// 십자 회전
+	for (int i = 1; i <= n; ++i) {
+		if (i == limit) continue;
+		v.push_back(board[i][limit]);
+	}
+	for (int i = 1; i <= n; ++i) {
+		if (i == limit) continue;
+		v.push_back(board[limit][i]);
+	}
+	for (int i = 1; i <= n; ++i) {
+		if (i == limit) continue;
+		board[limit][i] = v[index++];
+	}
+	for (int i = n; i > 0; --i) {
+		if (i == limit) continue;
+		board[i][limit] = v[index++];
+	}
+	// 정사각형 회전
+	v.clear();
+	index = 0;
+	for (int i = 1; i < limit; ++i)
+		for (int j = 1; j < limit; ++j)
+			v.push_back(board[i][j]);
+	for (int i = limit - 1; i > 0; --i)
+		for (int j = 1; j < limit; ++j)
+			board[j][i] = v[index++];
+
+	v.clear();
+	index = 0;
+	for (int i = 1; i < limit; ++i)
+		for (int j = limit + 1; j <= n; ++j)
+			v.push_back(board[i][j]);
+	for (int i = n; i > limit; --i)
+		for (int j = 1; j < limit; ++j)
+			board[j][i] = v[index++];
+
+	v.clear();
+	index = 0;
+	for (int i = limit + 1; i <= n; ++i)
+		for (int j = 1; j < limit; ++j)
+			v.push_back(board[i][j]);
+	for (int i = limit - 1; i > 0; --i)
+		for (int j = limit + 1; j <= n; ++j)
+			board[j][i] = v[index++];
+
+	v.clear();
+	index = 0;
+	for (int i = limit + 1; i <= n; ++i)
+		for (int j = limit + 1; j <= n; ++j)
+			v.push_back(board[i][j]);
+	for (int i = n; i > limit; --i)
+		for (int j = limit + 1; j <= n; ++j)
+			board[j][i] = v[index++];
+}
+void calc() {
+	map<pair<int, int>, int> check, check2;
+	for (int x = 1; x <= n; ++x) {
+		for (int y = 1; y <= n; ++y) {
+			for (int k = 0; k < 4; ++k) {
+				int nx = x + dx[k];
+				int ny = y + dy[k];
+				int now = visit[x][y];
+				int next = visit[nx][ny];
+				if (0 < nx && nx <= n && 0 < ny && ny <= n && now != next) {
+					if (!check[mp(now, next)]) check[mp(now, next)] = 1;
+					else ++check[mp(now, next)];
+				}
+			}
+		}
+	}
+	for (auto it = check.begin(); it != check.end(); ++it) {
+		int start = it->first.first;
+		int dest = it->first.second;
+		if (!check2[mp(start, dest)] && !check2[mp(dest, start)]) { // 2->3 , 3->2 와 같은 중복 방지
+			check2[mp(start, dest)] = check2[mp(dest, start)] = 1;
+			int sum = (group[start].second + group[dest].second) * group[start].first * group[dest].first * check[mp(start, dest)];
+			result += sum;
+		}
+	}
+}
+void make_group(int sx, int sy, int num) {
+	queue<pair<int, int> > q;
+	q.push(mp(sx, sy));
+	visit[sx][sy] = group_num;
+	int cnt = 0;
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second; q.pop();
+		++cnt;
+		for (int i = 0; i < 4; ++i) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (0 < nx && nx <= n && 0 < ny && ny <= n) {
+				if (!visit[nx][ny] && board[nx][ny] == num) {
+					q.push(mp(nx, ny));
+					visit[nx][ny] = group_num;
+				}
+			}
+		}
+	}
+	group.push_back(mp(num, cnt));
+}
+void start() {
+	for (int t = 1; t <= 4; ++t) {
+		memset(visit, 0, sizeof(visit));
+		group.clear();
+		group.push_back(mp(-1, -1));
+		group_num = 1;
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= n; ++j) {
+				if (!visit[i][j]) {
+					make_group(i, j, board[i][j]);
+					++group_num;
+				}
+			}
+		}
+		calc();
+		turn();
+	}
+	cout << result;
+}
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> n;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			cin >> board[i][j];
+		}
+	}
+	start();
+}
